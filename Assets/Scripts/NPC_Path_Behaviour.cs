f901fe26dbf5c438fb3b672a19cdcbff3ab082ff
Thomas S.T. Andersen <tsta19@student.aau.dk>
2022-03-08 15:52:34 +0100

    Added NPC_Path_Behaviour

--- Assets/Scripts/NPC_Path_Behaviour.cs
+++ Assets/Scripts/NPC_Path_Behaviour.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NPC_Path_Behaviour : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+using System; 
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.AI; 
+using UnityEngine.UI; 
+ 
+public class NPC_Path_Behaviour : MonoBehaviour 
+{ 
+    [SerializeField] private Transform[] waypoints; 
+    [SerializeField] private float moveSpeed = 2f; 
+ 
+    private int waypointIndex = 0; 
+     
+    void Start() 
+    { 
+        transform.position = waypoints[waypointIndex].transform.position; 
+    } 
+ 
+    void Update() 
+    { 
+        Move(); 
+    } 
+ 
+    void Move() 
+    { 
+        if (waypointIndex <= waypoints.Length - 1) 
+        { 
+            transform.position = Vector3.MoveTowards(transform.position, 
+                waypoints[waypointIndex].transform.position, 
+                moveSpeed * Time.deltaTime); 
+ 
+            if (transform.position == waypoints[waypointIndex].transform.position) 
+            { 
+                waypointIndex += 1; 
+            } 
+        } 
+    } 
 }
